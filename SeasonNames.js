9d02f37b09d8484ce57fe49a0686a6ccf1caadfc
denniskalpedis <dennis.kalpedis@gmail.com>
2019-01-02 19:45:05 -0600

    fixed season names not showing up sometimes.

--- SeasonNames.js
+++ SeasonNames.js
@@ -10,8 +10,9 @@
 // ==/UserScript==
 $('body').arrive('.form-group', function(){
     if($(".form-group").attr("data-attr") == "summary" && $(".modal-body-pane form").children().length == 1){
-        let name = $('[class^="PageHeaderLeft"] button').html();
-        name = name.split('<div ')[0];
+        let name = $(".modal-title").html();
+        let courseName = $('[class^="PageHeaderBreadcrumbButton"]').html();
+        name = name.substring(name.indexOf(courseName) + courseName.length + 3);
         $(".modal-body-pane form").prepend("<div class='row'><div class='col-md-12'><div class='form-group selectize-group' data-attr='name'><label for='lockable-name'>Name</label><div class='input-group'><a href='#' data-field='summary' class='input-group-addon edit-lock-addon' tabindex='-1'><i class='glyphicon lock'></i></a><input class='form-control lockable-field' id='lockable-name' name='title' value='" + name.replace("'", "&apos;").replace("\"", "&quot;") + "' /></div></div></div></div>");
     }
 });
