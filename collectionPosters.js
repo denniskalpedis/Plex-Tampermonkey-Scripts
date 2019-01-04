9b4cd6f5e0c14df60258fbc5ea64e819972dfa9a
denniskalpedis <dennis.kalpedis@gmail.com>
2019-01-03 19:53:33 -0600

    added background images also.

--- collectionPosters.js
+++ collectionPosters.js
@@ -9,53 +9,103 @@
 // @require     https://raw.githubusercontent.com/uzairfarooq/arrive/master/minified/arrive.min.js
 // ==/UserScript==
 
-var tmdbAPI = "<TheMovieDB API KEY HERE>";
+const tmdbAPI = "<TheMovieDB API KEY HERE>";
 // set fanArt to false to not use it.
-// I don't have a working FanART API KEY, can't test. (should work!)
-var fanArt = false;
-var fanArtAPI = "<FanArt API KEY HERE>";
-var language = "en";
+const fanArt = false;
+const fanArtAPI = "<FanArt API KEY HERE>";
+const language = "en";
 // posible poster sizes w92, w154, w185, w342, w500, w780, original
-// this is the poster size it downloads to save to plex from TheMovieDB
-var posterSize = "w342";
-var checking = false;
-
-$('body').arrive('.artwork-options-list', function(){
-    $(this).arrive('.poster', {onceOnly:true}, function(){
-        // might be overkill, but want to make sure you are in a collection edit window
-        if($(".poster-btn").hasClass("selected") && $(".modal-nav-list").children().length === 6 && $.trim($(".modal-nav-list").children()[2].textContent) === "Poster"){
-            let name = encodeURI($(".modal-title").html().split("Edit ")[1]);
-            var settings = {
-                "async": true,
-                "crossDomain": true,
-                "url": "https://api.themoviedb.org/3/search/collection?api_key=" + tmdbAPI + "&language=" + language + "&query=" + name + "&page=1",
-                "method": "GET",
-                "headers": {},
-                "data": "{}"
-            }
-            // was having a weird issue where it ran multiple times this check just lets it run the api calls once.
-            if(!checking){
-                checking = true;
-                // first call to TMDB to get collection ID
-                $.ajax(settings).done(function (response) {
-                    settings.url = "https://api.themoviedb.org/3/collection/" + response.results[0].id + "/images?api_key=" + tmdbAPI + "&language=" + language;
-                    // second call to TMDB to get collection posters
-                    $.ajax(settings).done(function (response) {
-                        for(let i = 0; i < response.posters.length; i++){
-                            $('.artwork-options-list').append('<span class="poster"><a class="artwork-option media-poster-container" data-rating-key="https://image.tmdb.org/t/p/' + posterSize + response.posters[i].file_path + '" href="#"> <div class="media-poster"><img class="media-poster-image loaded" src="https://image.tmdb.org/t/p/w154' + response.posters[i].file_path + '"></div> </a> </span>')
-                        }
-                        if(fanArt){
-                           settings.url = "https://webservice.fanart.tv/v3/movies/" + response.id + "?api_key=" + fanArtAPI;
-                            $.ajax(settings).done(function (response) {
-                                $('.artwork-options-list').append('<span class="poster"><a class="artwork-option media-poster-container" data-rating-key="' + response.movieposter[i].url + '" href="#"> <div class="media-poster"><img class="media-poster-image loaded" src="' + response.movieposter[i].url.replace("fanart", "preview") + '"></div> </a> </span>')
-                            });
-                        }
-                        checking = false;
-                    });
-                });
+const posterSize = "w342";
+// possible background sizes w300, w780, w1280, original
+const backdropSize = "original";
+let tmdbResults;
+let fanartResults;
+$('body').leave('.modal-dialog', function () {
+    tmdbResults = undefined;
+    fanartResults = undefined;
+});
+$('body').arrive('.artwork-options-list', function () {
+    if ($('[data-pane="art"]').hasClass("selected") && $(".modal-nav-list").children().length === 6 && $.trim($(".modal-nav-list").children()[2].textContent) === "Poster") {
+        if (tmdbResults) {
+            updateBackgrounds();
+        } else {
+            hitAPIs(updateBackgrounds);
+        }
+    }
+    $(this).arrive('.poster [data-rating-key="default://"]', function () {
+        if ($(".poster-btn").hasClass("selected") && $(".modal-nav-list").children().length === 6 && $.trim($(".modal-nav-list").children()[2].textContent) === "Poster") {
+            if (tmdbResults) {
+                updatePosters();
+            } else {
+                hitAPIs(updatePosters);
             }
         }
     });
+    function hitAPIs(_callback) {
+        let name = encodeURI($(".modal-title").html().split("Edit ")[1]);
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://api.themoviedb.org/3/search/collection?api_key=" + tmdbAPI + "&language=" + language + "&query=" + name + "&page=1",
+            "method": "GET",
+            "headers": {},
+            "data": "{}"
+        }
+        $.ajax(settings).done(function (response) {
+            settings.url = "https://api.themoviedb.org/3/collection/" + response.results[0].id + "/images?api_key=" + tmdbAPI + "&language=null," + language;
+            $.ajax(settings).done(function (response) {
+                tmdbResults = response;
+                if (fanArt) {
+                    settings.url = "https://webservice.fanart.tv/v3/movies/" + response.id + "?api_key=" + fanArtAPI;
+                    $.ajax(settings).done(function (response) {
+                        fanartResults = response;
+                        _callback();
+                    });
+                } else {
+                    _callback();
+                }
+            });
+        });
 
+    }
+
+    function updatePosters() {
+        if (tmdbResults) {
+            if (tmdbResults.posters[0].file_path[0] === "/") {
+                let results = tmdbResults.posters.map(i => 'https://image.tmdb.org/t/p/' + posterSize + i.file_path);
+                for (let i = 0; i < results.length; i++) {
+                    $('.artwork-options-list').append('<span class="poster"><a class="artwork-option media-poster-container" data-rating-key="' + results[i] + '" href="#"> <div class="media-poster"><img class="media-poster-image loaded" src="' + results[i].replace(posterSize, "w154") + '"></div> </a> </span>')
+                }
+            }
+            if (fanartResults) {
+                let results = fanartResults.movieposter.map(i => i.url);
+                for (let i = 0; i < results.length; i++) {
+                    $('.artwork-options-list').append('<span class="poster"><a class="artwork-option media-poster-container" data-rating-key="' + results[i] + '" href="#"> <div class="media-poster"><img class="media-poster-image loaded" src="' + results[i].replace("fanart/movies", "preview/movies") + '"></div> </a> </span>');
+                }
+            }
+
+        }
+
+    }
+
+    function updateBackgrounds() {
+        if (tmdbResults) {
+            if (tmdbResults.backdrops[0].file_path[0] === "/") {
+                let results = tmdbResults.backdrops.map(i => 'https://image.tmdb.org/t/p/' + backdropSize + i.file_path);
+                for (let i = 0; i < results.length; i++) {
+                    $('.artwork-options-list').append('<span class="art"><a class="artwork-option media-poster-container" data-rating-key="' + results[i] + '" href="#"> <div class="media-poster"><img class="media-poster-image loaded" src="' + results[i].replace(posterSize, "w300") + '"></div> </a> </span>')
+                }
+            }
+            // can't test yet....
+            if (fanartResults) {
+                let results = fanartResults.moviebackground.map(i => i.url);
+                for (let i = 0; i < results.length; i++) {
+                    $('.artwork-options-list').append('<span class="poster"><a class="artwork-option media-poster-container" data-rating-key="' + results[i] + '" href="#"> <div class="media-poster"><img class="media-poster-image loaded" src="' + results[i].replace("fanart/movies", "preview/movies") + '"></div> </a> </span>');
+                }
+            }
+
+        }
+
+    }
 
 });
