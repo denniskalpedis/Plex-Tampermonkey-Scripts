0ef7fe36e851ca176102c48ee32d671680ff67a5
denniskalpedis <dennis.kalpedis@gmail.com>
2020-11-18 23:59:33 -0600

    fix for movies with ' in title.

--- IMDbLinks.js
+++ IMDbLinks.js
@@ -14,9 +14,9 @@
 $('body').arrive('button[data-qa-id="preplay-trailer"]', function () {
     $('body').arrive("div[class^='CriticRating-container']", function () {
         if($('[title="IMDb Rating"]').length > 0){
-            $('[title="IMDb Rating"] div:first-child').attr('onClick', 'window.open(\'https://duckduckgo.com/?q=!ducky+site:imdb.com ' + $('div[class^="PrePlayLeftTitle-leftTitle"]')[0].innerText + ' ' + $('div[data-qa-id="preplay-secondTitle"]')[0].innerText + '\', \'_blank\')')
+            $('[title="IMDb Rating"] div:first-child').attr('onClick', 'window.open(\'https://duckduckgo.com/?q=!ducky+site:imdb.com ' + $('div[class^="PrePlayLeftTitle-leftTitle"]')[0].innerText.replace('\'', '') + ' ' + $('div[data-qa-id="preplay-secondTitle"]')[0].innerText + '\', \'_blank\')')
         }else {
-            $("div[class^='CriticRating-container'] div:first-child").first().append('<div class="CriticRating-rating-2Jrl15" title="IMDb Rating" style="margin-left: 1em;"><div class="CriticRating-imdb-23PVnW CriticRating-ratingImage-3-Lxod" onclick="window.open(\'https://duckduckgo.com/?q=!ducky+site:imdb.com ' + $('div[class^="PrePlayLeftTitle-leftTitle"]')[0].innerText + ' ' + $('div[data-qa-id="preplay-secondTitle"]')[0].innerText + '\', \'_blank\')"></div>&nbsp;&nbsp;</div>');
+            $("div[class^='CriticRating-container'] div:first-child").first().append('<div class="CriticRating-rating-2Jrl15" title="IMDb Rating" style="margin-left: 1em;"><div class="CriticRating-imdb-23PVnW CriticRating-ratingImage-3-Lxod" onclick="window.open(\'https://duckduckgo.com/?q=!ducky+site:imdb.com ' + $('div[class^="PrePlayLeftTitle-leftTitle"]')[0].innerText.replace('\'', '') + ' ' + $('div[data-qa-id="preplay-secondTitle"]')[0].innerText + '\', \'_blank\')"></div>&nbsp;&nbsp;</div>');
         }
     });
     $('body').leave('button[aria-label="Play Trailer"]', function () {
