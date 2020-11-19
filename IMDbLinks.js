61f3534204efe75a18d91e71f6ff8f604249ba32
denniskalpedis <dennis.kalpedis@gmail.com>
2020-11-18 23:36:14 -0600

    fix for imdb link

--- IMDbLinks.js
+++ IMDbLinks.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        plex Movie IMDb Link
 // @namespace   Plex.tv
-// @version     1.0
+// @version     1.1
 // @include     http*://<Private IP to access Plex>/*
 // @include     http*://app.plex.tv/*
 // @grant       none
@@ -11,12 +11,12 @@
 // @require     https://raw.githubusercontent.com/uzairfarooq/arrive/master/minified/arrive.min.js
 // ==/UserScript==
 
-$('body').arrive('button[aria-label="Play Trailer"]', function () {
+$('body').arrive('button[data-qa-id="preplay-trailer"]', function () {
     $('body').arrive("div[class^='CriticRating-container']", function () {
         if($('[title="IMDb Rating"]').length > 0){
-            $('[title="IMDb Rating"] div:first-child').attr('onClick', 'window.open(\'https://duckduckgo.com/?q=!ducky+site:imdb.com ' + $('div[class^="PrePlayLeftTitle-leftTitle"]')[0].innerText + ' ' + $('div[class^="PrePlayLeftTitle-leftTitle-Ev1KGW"]')[1].innerText + '\', \'_blank\')')
+            $('[title="IMDb Rating"] div:first-child').attr('onClick', 'window.open(\'https://duckduckgo.com/?q=!ducky+site:imdb.com ' + $('div[class^="PrePlayLeftTitle-leftTitle"]')[0].innerText + ' ' + $('div[data-qa-id="preplay-secondTitle"]')[0].innerText + '\', \'_blank\')')
         }else {
-            $("div[class^='CriticRating-container'] div:first-child").first().append('<div class="CriticRating-rating-2Jrl15" title="IMDb Rating" style="margin-left: 1em;"><div class="CriticRating-imdb-23PVnW CriticRating-ratingImage-3-Lxod" onclick="window.open(\'https://duckduckgo.com/?q=!ducky+site:imdb.com ' + $('div[class^="PrePlayLeftTitle-leftTitle"]')[0].innerText + ' ' + $('div[class^="PrePlayLeftTitle-leftTitle-Ev1KGW"]')[1].innerText + '\', \'_blank\')"></div>&nbsp;&nbsp;</div>');
+            $("div[class^='CriticRating-container'] div:first-child").first().append('<div class="CriticRating-rating-2Jrl15" title="IMDb Rating" style="margin-left: 1em;"><div class="CriticRating-imdb-23PVnW CriticRating-ratingImage-3-Lxod" onclick="window.open(\'https://duckduckgo.com/?q=!ducky+site:imdb.com ' + $('div[class^="PrePlayLeftTitle-leftTitle"]')[0].innerText + ' ' + $('div[data-qa-id="preplay-secondTitle"]')[0].innerText + '\', \'_blank\')"></div>&nbsp;&nbsp;</div>');
         }
     });
     $('body').leave('button[aria-label="Play Trailer"]', function () {
