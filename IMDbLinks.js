1199d7e0fa1426d0fe306c357ae55bf37f1ba3fb
David Lee <4148025+dauheeIRL@users.noreply.github.com>
2023-06-05 16:28:13 +0100

    Update IMDbLinks.js
    work for tv shows also

--- IMDbLinks.js
+++ IMDbLinks.js
@@ -18,30 +18,42 @@
     $('body').arrive('button[data-testid="preplay-trailer"]', function () {
 
         $( document ).ready(function() {
+
+            var strDetails = $('a[class^="PosterCardLink-link-"]').attr('aria-label');
             if($('[title^="IMDb Rating"]').length > 0){
-                var strDetails = $('a[class^="PosterCardLink-link-"]').attr('aria-label');
+
                 var astrDetails = strDetails.split(', ');
 
                 $('[title^="IMDb Rating"] span:first-child').attr('onClick', 'window.open("https://www.imdb.com/search/title/?title=' + astrDetails[0] + '&year=' + astrDetails[1] + '&adult=include", \'_blank\')');
+
+                $('span[title^="IMDb Rating "]').hover(
+                    function () {
+                        $('span[title^="IMDb Rating "]').css("background-color", "yellow");
+                        $('span[title^="IMDb Rating "]').css('cursor','help');
+                    },
+                    function () {
+                        $('span[title^="IMDb Rating "]').css("background-color", "rgba(0, 0, 0, 0.3)");
+                    }
+                );
+
+            }else if($('[title^="TMDB Rating"]').length > 0){
+
+                $('[title^="TMDB Rating"] span:first-child').attr('onClick', 'window.open("https://www.imdb.com/search/title/?title=' + strDetails + '&year=' + $('span[class^="ineka90 ineka9v ineka99 _1duebfhfy"]')[0].innerText + '&adult=include", \'_blank\')');
+
+                $('span[title^="TMDB Rating "]').hover(
+                    function () {
+                        $('span[title^="TMDB Rating "]').css("background-color", "yellow");
+                        $('span[title^="TMDB Rating "]').css('cursor','help');
+                    },
+                    function () {
+                        $('span[title^="TMDB Rating "]').css("background-color", "rgba(0, 0, 0, 0.3)");
+                    }
+                );
 
             }
 
         });
 
-
-        $('span[title^="IMDb Rating "]').hover(
-            function () {
-                $('span[title^="IMDb Rating "]').css("background-color", "yellow");
-                $('span[title^="IMDb Rating "]').css('cursor','help');
-            },
-            function () {
-                $('span[title^="IMDb Rating "]').css("background-color", "rgba(0, 0, 0, 0.3)");
-            }
-        );
-
-
-
     });
-
 
 })(jQuery);
