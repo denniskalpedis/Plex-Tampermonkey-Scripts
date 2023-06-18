a788e9d7520c8fd8a551c5fe3c0e5d5ea102f173
David Lee <4148025+dauheeIRL@users.noreply.github.com>
2023-06-18 11:12:52 +0100

    Update ytsmoviecheck.js
    last tidy

--- ytsmoviecheck.js
+++ ytsmoviecheck.js
@@ -16,7 +16,7 @@
 
     function LoadMovieLocation(){
         const PLEX_TOKEN = 'xx';
-        const PLEX_SERVER = 'xxx';
+        const PLEX_SERVER = 'xx';
         var strMovieName = $('h1')[0].innerText;
         var strYear = $('h2')[0].innerText; //$('h1')[0].baseURI;
 
@@ -28,12 +28,12 @@
 
             if (strPlexData.length > 450){
                 var strVideoQual = strPlexData.split('videoResolution="')[1].split('"')[0]
-                $('h1').after('<h2 id="myinject" style="background-color:yellow;color:red">ADULTS MOVIE (' + strVideoQual + ')</h2>');
+                $('h1').after('<h2 style="background-color:yellow;color:red">ADULTS MOVIE (' + strVideoQual + ')</h2>');
             }
 
         });
 
-        var fetchDownloadLink = 'https://' + PLEX_SERVER + '/library/sections/1/all?X-Plex-Token=' + PLEX_TOKEN + '&year=' +strYear + '&title=' + strMovieName; //adults
+        fetchDownloadLink = 'https://' + PLEX_SERVER + '/library/sections/1/all?X-Plex-Token=' + PLEX_TOKEN + '&year=' +strYear + '&title=' + strMovieName; //adults
 
         $.get( fetchDownloadLink, function( data ) {
 
@@ -42,7 +42,7 @@
             if (strPlexData.length > 450){
                 var strVideoQual = strPlexData.split('videoResolution="')[1].split('"')[0]
 
-                $('h1').after('<h2 id="myinject" style="background-color:yellow;color:red">ADULTS MOVIE (' + strVideoQual + ')</h2>');
+                $('h1').after('<h2 style="background-color:yellow;color:red">ADULTS MOVIE (' + strVideoQual + ')</h2>');
             }
 
         });
@@ -50,12 +50,8 @@
     }
 
     $( document ).ready(function() {
-
-        setTimeout(LoadMovieLocation, 1500);
-
+        setTimeout(LoadMovieLocation, 1000);
     });
-
-
 
 
 })(jQuery);
