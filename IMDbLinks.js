6f67d35fb32e5b24a6a1798ac4f903f5e927d978
David Lee <4148025+dauheeIRL@users.noreply.github.com>
2023-06-29 14:09:43 +0100

    Update IMDbLinks.js

--- IMDbLinks.js
+++ IMDbLinks.js
@@ -23,7 +23,7 @@
 
         const strTitleHeader = 'h1[data-testid="metadata-title"]';
 
-        var currentpagelink = $('img[style^="position: absolute; width: 100%; height: 100%; object-fit: cover; opacity: 1;"]').attr('src');
+        var currentpagelink = $('div[data-testid="metadata-poster"] div img')[0].src ; //$('img[style^="position: absolute; width: 100%; height: 100%; object-fit: cover; opacity: 1;"]').attr('src');
         var movieid = currentpagelink.split('metadata%2F')[1].split('%2Fthumb')[0];
         var plextoken = currentpagelink.split('&X-Plex-Token=')[1];
         var fetchDownloadLink = currentpagelink.substr(0, getPosition(currentpagelink, '/', 3)) + '/library/metadata/' + movieid + '?X-Plex-Token=' + plextoken;
@@ -35,21 +35,34 @@
 
             var strPlexData = new XMLSerializer().serializeToString(data.documentElement);
 
-            if($('[title^="TMDB Rating"]').length > 0){
+            if($('[title^="IMDB Rating"]').length > 0){
 
-                //split by file location
-                strPlexData = strPlexData.split(' path="');
-                //in reverse order so get last file
-                strPlexData = strPlexData[strPlexData.length - 1].split('"')[0];
-                //chop of part as not required for SMB
-                strPlexData = strPlexData.split('/share/external/DEV3302_1/')[1];
-                strPlexData = "\\\\" + currentpagelink + "\\USBDrive1\\" + strPlexData //SMB format for windows
-
-            } else {
                 //get the file portion and remove last slash
                 strPlexData = strPlexData.split(' file="')[1].split(" size=")[0].slice(0, -1);
                 strPlexData = "\\\\" + currentpagelink + "\\Media\\" + strPlexData.substr(getPosition(strPlexData, '/', 4) + 1); //SMB format for windows
                 strPlexData = strPlexData.substr(0, strPlexData.lastIndexOf('/')); //just want to folder not the file as might delete
+
+            } else {
+
+                //split by path location - for TV series
+                strPlexData = strPlexData.split(' path="');
+
+                if (strPlexData[strPlexData.length - 1].split('"')[0].toUpperCase().includes('S01')){
+                    //in reverse order so get last file
+                    strPlexData = strPlexData[strPlexData.length - 1].split('"')[0];
+                } else{
+                    strPlexData = strPlexData[1].split('"')[0];
+                }
+
+                //chop of part as not required for SMB
+                if (strPlexData.toUpperCase().includes('/SHARE/EXTERNAL/DEV3302_1/')){
+                    strPlexData = strPlexData.split('/share/external/DEV3302_1/')[1];
+                    strPlexData = "\\\\" + currentpagelink + "\\USBDrive1\\" + strPlexData; //SMB format for windows
+                } else{
+                    strPlexData = strPlexData.substr(getPosition(strPlexData, '/', 4) + 1);
+                    strPlexData = "\\\\" + currentpagelink + "\\Media\\" + strPlexData; //SMB format for windows
+
+                }
 
             }
 
@@ -78,16 +91,18 @@
         var strMovieName = $('a[class^="PosterCardLink-link-"]').attr('aria-label');
         var strYear;
 
-        if($('[title^="TMDB Rating"]').length > 0){
-            //tv series
-            strYear = $('span[data-testid^="metadata-line1"]')[0].innerText;
-        } else {
+        if($('[title^="IMDB Rating"]').length > 0){
             var lastpos = strMovieName.lastIndexOf(', ') + 2;
             strYear = strMovieName.slice(lastpos);
-            strMovieName = strMovieName.substr(0, lastpos - 2).replace(' (' + strYear + ')', '');
+            strMovieName = strMovieName.substr(0, lastpos - 2);
+
+        } else {
+            //tv series
+            strYear = $('span[data-testid^="metadata-line1"]')[0].innerText;
         }
 
-        //$('[title^="IMDb Rating"]' + ' span:first-child') '[title^="Rotten Tomatoes Audience Rating "]'
+        strMovieName = strMovieName.replace(' (' + strYear + ')', ''); //don't want year at end
+
         $(strRatingSection).attr('onClick', 'window.open("https://www.imdb.com/search/title/?title=' + strMovieName.replaceAll(' ', '+') + '&release_date=' + (parseInt(strYear) - 1) + '-01-01,' + (parseInt(strYear) + 1) + '-01-01&adult=include&sort=year,desc", \'_blank\')');
 
         $(strRatingSection).hover(
